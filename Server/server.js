799fcfc109be5cf36f2c988e3017b31cec007051
Muhammad Tayyab <66192491+Muhammad-Tayyab1@users.noreply.github.com>
2020-11-21 11:02:10 +0500

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -29,14 +29,14 @@ app.get('/api/config/paypal', (req, res) => {
 });
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-app.use(express.static(path.join(__dirname, '/../client/build')));
+app.use(express.static(path.join(__dirname, '/client/build')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/../client/build/index.html`));
+  res.sendFile(path.join(`${__dirname}/client/build/index.html`));
 });
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
 app.listen(config.PORT, () => {
-  console.log('Server started at http://localhost:5000');
+  console.log(`Server started at http://localhost:${config.PORT}`);
 });
