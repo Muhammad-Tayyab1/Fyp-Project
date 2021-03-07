8350aa6fe8238b98d3127eaba5646c30e080fc00
Muhammad Tayyab <66192491+Muhammad-Tayyab1@users.noreply.github.com>
2021-03-07 19:13:28 +0500

    Pwa Added

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,26 @@ import  Navigation  from './components/navigation';
 import React from 'react';
 import './App.css';
 import { Routers } from './Routers';
+import firebase from './FireBaseWork/firebase';
 function App() {
+  const messaging=firebase.messaging();
+    Notification.requestPermission().then((permission) => { 
+        console.log(permission) 
+        if(permission === "granted"){
+            messaging.getToken().then((currentToken) => {
+                if (currentToken) {
+                    console.log("TOKEN")
+                    console.log(currentToken);
+                } else {
+                  console.log('No Instance ID token available. Request permission to generate one.');
+
+                }
+              }).catch((err) => {
+                console.log('An error occurred while retrieving token. ', err);
+              });
+        }
+  
+    })
   return (
     <div>
       <Navigation/>
